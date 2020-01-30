ac01a7774a3cc0a06346cf17368769ee643de517
ghXplorer <57403493+ghXplorer@users.noreply.github.com>
2020-01-30 10:54:09 +0300

    Delete init_cmd.sh

--- vpn-srv-aws/init_cmd.sh
+++
@@ -1,2 +0,0 @@
-# this command is issued on Linux host with AWS CLI installed
-curl -s https://raw.githubusercontent.com/AWSinAction/code2/master/chapter05/vpn-create-cloudformation-stack.sh | bash -ex
